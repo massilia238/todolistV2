25a97cd9e359ad5a4b4427b10e7382cff7ca7e0e
massilia <megdoudmassilia@gmail.com>
2026-04-30 12:29:53 +0100

    prepare vercel deployment

--- config/settings.py
+++ config/settings.py
@@ -25,8 +25,7 @@ SECRET_KEY = 'django-insecure--h+*_x!1zz%c_lo!k!*3q#p853cnm_uzd2heh4&wooggp33^36
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = ["*"]
 
 # Application definition
 
@@ -117,3 +116,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
