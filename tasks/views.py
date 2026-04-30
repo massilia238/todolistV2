d371c948c9a85d1b3c386965d8e79c421b0b8ab0
massilia <megdoudmassilia@gmail.com>
2026-04-30 13:33:14 +0100

    build todo app UI + CRUD features

--- tasks/views.py
+++ tasks/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from .models import Task
 
-# Create your views here.
+def index(request):
+    if request.method == "POST":
+        title = request.POST.get("title")
+        if title:
+            Task.objects.create(title=title)
+        return redirect('index')
+
+    tasks = Task.objects.all().order_by('-created')
+    return render(request, "tasks/index.html", {"tasks": tasks})
+
+
+def delete_task(request, task_id):
+    task = get_object_or_404(Task, id=task_id)
+    task.delete()
+    return redirect('index')
+
+
+def toggle_task(request, task_id):
+    task = get_object_or_404(Task, id=task_id)
+    task.completed = not task.completed
+    task.save()
+    return redirect('index')
